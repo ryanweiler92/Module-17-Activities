28851ff2a4916f6c8e13d55c0fe0d5cb89f8b3ac
ryanweiler92 <ryanweiler92@gmail.com>
2022-03-15 10:15:23 -0400

    files

---
+++ 17-CS/01-Activities/01-Closures/Unsolved/index.js
@@ -0,0 +1,16 @@
+// TODO: Complete the 'counter' function below.
+function counter() {
+    let count = 0;
+    return function() {
+     return count++;
+    }
+};
+
+const increment = counter()
+
+increment();
+
+// Displays an interactive list of the properties of the specified JavaScript object
+console.dir(counter);
+
+module.exports = counter;
